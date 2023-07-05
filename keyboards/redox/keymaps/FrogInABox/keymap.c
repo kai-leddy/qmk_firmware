4804464d860fb70b3575f130931aa58ba593eb83
Kai Leddy <epickaimail@gmail.com>
2023-07-05 15:48:58 +0100

    Just use normal caps lock now and let Hyper.app handle it

--- keyboards/redox/keymaps/FrogInABox/keymap.c
+++ keyboards/redox/keymaps/FrogInABox/keymap.c
@@ -16,32 +16,24 @@
 /* }; */
 
 // Shortcuts for layer selection mods
-#define MED_RGB MO(_MEDRGB) // switch to media & rgb layer whilst held
+#define MED_RGB MO(_MEDRGB)         // switch to media & rgb layer whilst held
 #define SYM_SPC LT(_SYMBOL, KC_SPC) // switch to symbol layer whilst held, K_SPC when tapped
 
 // Shortcuts for keypress mods
-#define CTL_ESC CTL_T(KC_ESC) // ESC on tap, CTL on hold
-#define GUI_ENT GUI_T(KC_ENTER) // ENTER on tap, GUI on hold
+// #define CTL_ESC CTL_T(KC_ESC) // ESC on tap, CTL on hold
+#define GUI_ENT GUI_T(KC_ENTER)  // ENTER on tap, GUI on hold
 #define BSLS_SFT RSFT_T(KC_BSLS) // Backslask on tap, RShift on hold
 
 // Unicode & Emoji key names
-enum unicode_names {
-   JOY,
-   SWEAT,
-   SMILE,
-   THUMB,
-   OKAY,
-   HUND,
-   TADA
-};
+enum unicode_names { JOY, SWEAT, SMILE, THUMB, OKAY, HUND, TADA };
 const uint32_t PROGMEM unicode_map[] = {
-   [JOY] = 0x1F602, // 😂
-   [SWEAT] = 0x1F605, // 😅
-   [SMILE] = 0x1F642, // 🙂
-   [THUMB] = 0x1F44D, // 👍
-   [OKAY] = 0x1F44C, // 👌
-   [HUND] = 0x1F4AF, // 💯
-   [TADA] = 0x1F389, // 🎉
+    [JOY]   = 0x1F602, // 😂
+    [SWEAT] = 0x1F605, // 😅
+    [SMILE] = 0x1F642, // 🙂
+    [THUMB] = 0x1F44D, // 👍
+    [OKAY]  = 0x1F44C, // 👌
+    [HUND]  = 0x1F4AF, // 💯
+    [TADA]  = 0x1F389, // 🎉
 };
 
 // clang-format off
@@ -53,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_DEL  ,                          KC_BSPC ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_PIPE ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     CTL_ESC ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_EQL  ,                          KC_MINS ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOTE,
+     KC_CAPS ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_EQL  ,                          KC_MINS ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOTE,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_PGUP ,KC_PGDN ,        KC_HOME ,KC_END  ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,BSLS_SFT,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
